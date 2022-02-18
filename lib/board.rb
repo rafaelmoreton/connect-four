0ed6380fbd36d0e453348d483ffe1c11aa79d09c
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-02-18 20:24:37 -0300

    Feat: add Board class and it's tests
    Keep track of the state of the game, as well as control the piece drops and
    check for victory condition.

---
+++ lib/board.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+# Used to create, manipulate and check an array structure with elements that
+# represent the slots of the game board.
+class Board
+  attr_reader :slots, :last_play
+
+  def initialize
+    @slots = Array.new(42) { |i| i + 1 }
+  end
+
+  def drop_piece(column, piece)
+    bottom_slot_index = find_bottom(column)
+    @slots[bottom_slot_index] = piece
+    @last_play = bottom_slot_index
+  end
+
+  def find_bottom(column)
+    column_index = column - 1
+    line_multiplier = 6
+    6.times do
+      line_multiplier -= 1
+      bottom = @slots[column_index + (line_multiplier * 7)]
+      return @slots.index(bottom) if bottom.instance_of?(Integer)
+    end
+  end
+
+  def game_over?
+    [row_win?, column_win?, diagonal_win?].any?(true)
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Style/EmptyElse
+  def row_win?
+    return false if defined?(@last_play).nil?
+
+    right_row = []
+    4.times do |i|
+      right_row << @slots[@last_play + i]
+    end
+
+    left_row = []
+    4.times do |i|
+      left_row <<
+        if (@last_play - i).positive?
+          @slots[@last_play - i]
+        else
+          nil
+        end
+    end
+
+    [right_row, left_row].any? do |winning_array|
+      winning_array.uniq.length == 1
+    end
+  end
+
+  def diagonal_win?
+    return false if defined?(@last_play).nil?
+
+    up_right_diagonal = []
+    4.times do |i|
+      up_right_diagonal <<
+        if (@last_play - (i * 6)).positive?
+          @slots[@last_play - (i * 6)]
+        else
+          nil
+        end
+    end
+
+    up_left_diagonal = []
+    4.times do |i|
+      up_left_diagonal <<
+        if (@last_play - (i * 8)).positive?
+          @slots[@last_play - (i * 8)]
+        else
+          nil
+        end
+    end
+
+    down_right_diagonal = []
+    4.times do |i|
+      down_right_diagonal << @slots[@last_play + (i * 8)]
+    end
+
+    down_left_diagonal = []
+    4.times do |i|
+      down_left_diagonal << @slots[@last_play + (i * 6)]
+    end
+
+    [up_right_diagonal, up_left_diagonal, up_right_diagonal, up_left_diagonal]
+      .any? { |winning_array| winning_array.uniq.length == 1 }
+  end
+  # rubocop:enable Style/EmptyElse
+  # rubocop:enable Metrics/MethodLength
+
+  def column_win?
+    return false if defined?(@last_play).nil?
+
+    column = []
+    4.times { |i| column << @slots[@last_play + (i * 7)] }
+    column.uniq.length == 1
+  end
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,250 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board'
+
+# rubocop:disable Metrics/BlockLength
+describe Board do
+  subject(:board) { described_class.new }
+
+  describe '#find_bottom' do
+    context 'when passed "5" as argument' do
+      it 'returns index of bottom slot of column 5' do
+        bottom_slot_index = board.find_bottom(5)
+        expect(bottom_slot_index).to eq 39
+      end
+    end
+
+    context 'when passed "7" as argument' do
+      it 'returns index of bottom slot of column 7' do
+        bottom_slot_index = board.find_bottom(7)
+        expect(bottom_slot_index).to eq 41
+      end
+    end
+  end
+
+  describe '#drop_piece' do
+    context 'when passed "3" as argument' do
+      it 'fills the bottom slot of column "3"' do
+        expect { board.drop_piece(3, 'x') }.to change { board.slots[37] }.from(38).to('x')
+      end
+      context 'if the bottom slot of the column is taken' do
+        it 'fills the bottommost empty slot' do
+          board.drop_piece(3, 'x')
+          expect { board.drop_piece(3, 'x') }.to change { board.slots[30] }.from(31).to('x')
+        end
+      end
+    end
+  end
+
+  describe '#game_over?' do
+    context 'at game start' do
+      it 'returns false' do
+        result = board.game_over?
+        expect(result).to be false
+      end
+    end
+
+    context 'when the board is full, but there is no 4-pieces-line' do
+      it 'returns false' do
+        full_board = %w[
+          o o x o o x o
+          x x x o x o o
+          x o x o x x o
+          o x o x x o x
+          o x o x o o x
+          o o o x o o x
+        ]
+        board.instance_variable_set(:@slots, full_board)
+        result = board.game_over?
+        expect(result).to be false
+      end
+    end
+
+    context 'when there is a 4-pieces-line' do
+      it 'returns true' do
+        4.times { board.drop_piece(2, 'x') }
+        result = board.game_over?
+        expect(result).to be true
+      end
+    end
+  end
+
+  describe '#row_win?' do
+    context 'at game start' do
+      it 'returns false' do
+        result = board.row_win?
+        expect(result).to be false
+      end
+    end
+
+    context "after piece drops that don't lead to victory" do
+      it 'returns false' do
+        board.drop_piece(1, 'x')
+        board.drop_piece(2, 'x')
+        3.times { board.drop_piece(4, 'x') }
+        board.drop_piece(6, 'x')
+        board.drop_piece(7, 'x')
+        result = board.row_win?
+        expect(result).to be false
+      end
+    end
+
+    context 'when the ends of @slots could wrap, leading to false victory' do
+      it 'returns false' do
+        board.drop_piece(5, 'x')
+        board.drop_piece(6, 'x')
+        board.drop_piece(7, 'x')
+        6.times { board.drop_piece(1, 'x') }
+        6.times { board.drop_piece(2, 'x') }
+        result = board.row_win?
+        expect(result).to be false
+      end
+    end
+
+    context 'when a 4-pieces-line is formed in a row' do
+      it 'returns true' do
+        board.drop_piece(2, 'x')
+        board.drop_piece(3, 'x')
+        board.drop_piece(4, 'x')
+        board.drop_piece(5, 'x')
+        result = board.row_win?
+        expect(result).to be true
+      end
+    end
+  end
+
+  describe '#column_win?' do
+    context 'at game start' do
+      it 'returns false' do
+        result = board.column_win?
+        expect(result).to be false
+      end
+    end
+
+    context "after piece drops that don't lead to victory" do
+      it 'returns false' do
+        board.drop_piece(1, 'x')
+        board.drop_piece(2, 'x')
+        3.times { board.drop_piece(4, 'x') }
+        board.drop_piece(6, 'x')
+        board.drop_piece(7, 'x')
+        result = board.column_win?
+        expect(result).to be false
+      end
+    end
+
+    context 'when the ends of @slots could wrap, leading to false victory' do
+      it 'returns false' do
+        3.times do
+          board.drop_piece(1, 'x')
+          board.drop_piece(2, 'x')
+          board.drop_piece(3, 'x')
+          board.drop_piece(5, 'x')
+          board.drop_piece(6, 'x')
+          board.drop_piece(7, 'x')
+        end
+        board.drop_piece(1, 'o')
+        board.drop_piece(2, 'o')
+        board.drop_piece(3, 'o')
+        board.drop_piece(5, 'o')
+        board.drop_piece(6, 'o')
+        board.drop_piece(7, 'o')
+        3.times do
+          board.drop_piece(1, 'x')
+          board.drop_piece(2, 'x')
+          board.drop_piece(3, 'x')
+          board.drop_piece(5, 'x')
+          board.drop_piece(6, 'x')
+          board.drop_piece(7, 'x')
+        end
+        result = board.column_win?
+        expect(result).to be false
+      end
+    end
+
+    context 'when a 4-pieces-line is formed in a column' do
+      it 'returns true' do
+        4.times { board.drop_piece(2, 'x') }
+        result = board.column_win?
+        expect(result).to be true
+      end
+    end
+  end
+
+  describe '#diagonal_win?' do
+    context 'at game start' do
+      it 'returns false' do
+        result = board.diagonal_win?
+        expect(result).to be false
+      end
+    end
+
+    context "after piece drops that don't lead to victory" do
+      it 'returns false' do
+        board.drop_piece(2, 'o')
+        board.drop_piece(3, 'o')
+        board.drop_piece(4, 'o')
+        board.drop_piece(4, 'o')
+        board.drop_piece(4, 'x')
+        board.drop_piece(3, 'x')
+        2.times do
+          board.drop_piece(5, 'x')
+          board.drop_piece(6, 'x')
+          board.drop_piece(7, 'x')
+        end
+        board.drop_piece(5, 'o')
+        board.drop_piece(6, 'o')
+        board.drop_piece(6, 'x')
+        3.times do
+          board.drop_piece(7, 'o')
+        end
+        result = board.diagonal_win?
+        expect(result).to be false
+      end
+    end
+
+    context 'when the ends of @slots could wrap, leading to false victory' do
+      it 'returns false' do
+        3.times do
+          board.drop_piece(1, 'x')
+          board.drop_piece(2, 'x')
+          board.drop_piece(3, 'x')
+          board.drop_piece(5, 'x')
+          board.drop_piece(6, 'x')
+          board.drop_piece(7, 'x')
+        end
+        board.drop_piece(1, 'o')
+        board.drop_piece(2, 'o')
+        board.drop_piece(3, 'o')
+        board.drop_piece(5, 'o')
+        board.drop_piece(6, 'o')
+        board.drop_piece(7, 'o')
+        3.times do
+          board.drop_piece(1, 'x')
+          board.drop_piece(2, 'x')
+          board.drop_piece(3, 'x')
+          board.drop_piece(5, 'x')
+          board.drop_piece(6, 'x')
+          board.drop_piece(7, 'x')
+        end
+        result = board.diagonal_win?
+        expect(result).to be false
+      end
+    end
+
+    context 'when a 4-pieces-line is formed in a diagonal' do
+      it 'returns true' do
+        3.times { board.drop_piece(2, 'x') }
+        2.times { board.drop_piece(3, 'x') }
+        board.drop_piece(4, 'x')
+        board.drop_piece(2, 'o')
+        board.drop_piece(3, 'o')
+        board.drop_piece(4, 'o')
+        board.drop_piece(5, 'o')
+        result = board.diagonal_win?
+        expect(result).to be true
+      end
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
